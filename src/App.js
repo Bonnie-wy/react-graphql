850bdb70daf251b67e6bf59504867439c783d867
Bonnie-wy <waiyeewong89@gmail.com>
2022-03-03 17:08:21 +0000

    added search component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import CharacterList from "./pages/CharacterList";
 import Character from "./pages/Character";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <Routes>
       <Route strict exact path="/" element={<CharacterList />} />
+      <Route strict exact path="/search" element={<Search />} />
       <Route strict exact path="/:id" element={<Character />} />
     </Routes>
   );
